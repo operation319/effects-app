fcdfc959c8eb0a17e0fe559470de91941fb3c49f
[Omar Vecino] <[Omar-vecino@hotmail.com]>
2021-07-08 09:42:18 -0500

    primer commit app sin effects

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private url = 'https://reqres.in/api';
+
+  constructor( private http: HttpClient ) { }
+
+  getUsers(): any {
+   return this.http.get(`${ this.url }/users?per_page=6`)
+          .pipe(
+            // tslint:disable-next-line: no-string-literal
+            map( resp => resp ['data'])
+          );
+  }
+
+}
